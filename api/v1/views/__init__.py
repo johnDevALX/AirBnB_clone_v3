525f46a5786fb6f43f9f26f552805e7d13c60615
johnDevALX <jj7014612@gmail.com>
2023-05-01 12:43:59 -0700

    API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-'''Contains the blueprint for the API.'''
-from flask import Blueprint
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-'''The blueprint for the AirBnB clone API.'''
+app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
+app.register_blueprint(app_views)
 
 
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.index import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
+@app.teardown_appcontext
+def tear_down(self):
+    '''
+    close query after each session
+    '''
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
